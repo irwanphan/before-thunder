9fd0dddb35a1a15b8fe7bdf212affdb966e90387
irwanphan <irwanphan@gmail.com>
2024-03-06 23:37:35 +0700

    navbar links

---
+++ app/teaching-class/page.tsx
@@ -0,0 +1,11 @@
+
+
+const TeachingClassPage = () => {
+  return (
+    <div>
+      <h1>Teaching Class</h1>
+    </div>
+  );
+}
+
+export default TeachingClassPage;
--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -4,6 +4,7 @@ import { Menu, Group, Center, Burger, Container } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { IconChevronDown } from '@tabler/icons-react';
 import classes from './Navbar.module.css';
+import Link from 'next/link';
 
 const links = [
   { link: '/dashboard', label: 'Dashboard' },
@@ -21,58 +22,56 @@ const links = [
 ];
 
 export function Navbar() {
-  const [opened, { toggle }] = useDisclosure(false);
+    const [opened, { toggle }] = useDisclosure(false);
 
-  const items = links.map((link) => {
-    const menuItems = link.links?.map((item) => (
-      <Menu.Item key={item.link}>{item.label}</Menu.Item>
-    ));
+    const items = links.map((link) => {
+        const menuItems = link.links?.map((item) => (
+            <Menu.Item key={item.label}>
+                <Link href={item.link} passHref className={classes.link}>
+                    {item.label}
+                </Link>
+            </Menu.Item>
+        ));
 
-    if (menuItems) {
-      return (
-        <Menu key={link.label} trigger="hover" transitionProps={{ exitDuration: 0 }} withinPortal>
-          <Menu.Target>
-            <a
-              href={link.link}
-              className={classes.link}
-              onClick={(event) => event.preventDefault()}
+        if (menuItems) {
+            return (
+                <Menu key={link.label} trigger="hover" transitionProps={{ exitDuration: 0 }} withinPortal>
+                    <Menu.Target>
+                        <Center>
+                            <span className={classes.link}>{link.label}</span>
+                            <IconChevronDown size="0.9rem" stroke={1.5} />
+                        </Center>
+                    </Menu.Target>
+                    <Menu.Dropdown>{menuItems}</Menu.Dropdown>
+                </Menu>
+            );
+        }
+
+        return (
+            <Link passHref
+                key={link.label}
+                href={link.link}
+                className={classes.link}
+                // onClick={(event) => event.preventDefault()}
             >
-              <Center>
-                <span className={classes.linkLabel}>{link.label}</span>
-                <IconChevronDown size="0.9rem" stroke={1.5} />
-              </Center>
-            </a>
-          </Menu.Target>
-          <Menu.Dropdown>{menuItems}</Menu.Dropdown>
-        </Menu>
-      );
-    }
+                {link.label}
+            </Link>
+        );
+    });
 
     return (
-      <a
-        key={link.label}
-        href={link.link}
-        className={classes.link}
-        onClick={(event) => event.preventDefault()}
-      >
-        {link.label}
-      </a>
+        <header className={classes.header}>
+            <Container size="md">
+                <div className={classes.inner}>
+                    <span>the ASDF</span>
+                    <Group gap={5} visibleFrom="sm">
+                        {items}
+                    </Group>
+                    <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
+                </div>
+            </Container>
+        </header>
     );
-  });
-
-  return (
-    <header className={classes.header}>
-      <Container size="md">
-        <div className={classes.inner}>
-          <span>the ASDF</span>
-          <Group gap={5} visibleFrom="sm">
-            {items}
-          </Group>
-          <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
-        </div>
-      </Container>
-    </header>
-  );
 }
 
 export default Navbar;
