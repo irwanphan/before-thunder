91412667fe7e74c8dd674cfd87dea5c7373332d3
irwanphan <irwanphan@gmail.com>
2024-03-07 16:15:43 +0700

    link to create KelasAjar

--- app/teaching-class/page.tsx
+++ app/teaching-class/page.tsx
@@ -1,16 +1,24 @@
 import prisma from '@components/prisma';
 import TeachingClassTable from './_components/TeachingClassTable';
+import Link from 'next/link';
+import { Button, Flex, Text } from '@mantine/core';
 
 const TeachingClassPage = async () => {
-  const teachingClasses = await getTeachingClasses();
-  // { user: session?.user }
-  // console.log(teachingClasses);
+    const teachingClasses = await getTeachingClasses();
+    // { user: session?.user }
+    // console.log(teachingClasses);
 
-  return (
-    <div>
-      <TeachingClassTable teachingClasses={teachingClasses} />
-    </div>
-  )
+    return (
+        <div>
+            <Flex>
+                <Text size="lg" mr={16}>Daftar Kelas Ajar</Text>
+                <Link href="/teaching-class/create" passHref>
+                    <Button>Tambah Baru</Button>
+                </Link>
+            </Flex>
+            <TeachingClassTable teachingClasses={teachingClasses} />
+        </div>
+    )
 }
 
 async function getTeachingClasses() {
