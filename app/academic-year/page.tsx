ec15f37a85b72df0573120b1a5d1a8d62c7b710d
irwanphan <irwanphan@gmail.com>
2024-03-11 14:08:38 +0700

    AcademicYearsPage

---
+++ app/academic-year/page.tsx
@@ -0,0 +1,60 @@
+import prisma from "@components/prisma";
+import { authOptions } from "@lib/auth-options";
+import { Card, Flex, Button, Text } from "@mantine/core";
+import { getServerSession } from "next-auth";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+async function getAcademicYear(userId: number) {
+    return await prisma.academicYear.findMany(
+        { 
+            where: {
+                authorId: userId
+            }
+        }
+    );
+}
+
+const AcademicYearPage = async () => {
+    const session = await getServerSession(authOptions);
+    if (!session || session === null) {
+        redirect('/auth/signin');
+    }
+    // console.log('session: ', session);
+    const email = session?.user?.email;
+    if (!email) {
+        redirect('/auth/signin');
+    }
+
+    const user = await prisma.user.findUnique({
+        where: {
+        email,
+        },
+    });
+    if (!user) {
+        redirect('/auth/signin');
+    }
+    const academicYears = await getAcademicYear(user.id);
+    return (
+        <Card shadow="sm" padding="lg" radius="md" withBorder>
+        <Flex>
+            <Text size="lg" mr={16}>
+            Daftar Tahun Akademik
+            </Text>
+            <Link href="/department/create" passHref>
+            <Button>Tambah Baru</Button>
+            </Link>
+        </Flex>
+        {
+            academicYears.length === 0 ?
+            <Text mt={16}>
+                Belum ada tahun akademik yang dibuat.
+            </Text>
+            // : <TeachingClassTable academicYears={academicYears} />
+            : <></>
+        }
+        </Card>
+    );
+    }
+
+export default AcademicYearPage;
