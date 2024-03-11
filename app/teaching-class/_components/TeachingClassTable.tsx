f5e12c71a28bd794a4d706d4883ba5c26e080114
irwanphan <irwanphan@gmail.com>
2024-03-11 16:07:48 +0700

    lib tableStyles

--- app/teaching-class/_components/TeachingClassTable.tsx
+++ app/teaching-class/_components/TeachingClassTable.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Link from 'next/link';
-import classes from './TeachingClassTable.module.css';
+import classes from '@lib/tableStyles.module.css';
 
 type TeachingClassType = {
   id: number;
